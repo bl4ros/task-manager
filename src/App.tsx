6fc1018bd5dc84eafb8aea86180f6753cebfa9fe
fe-nunes <fernando.fnn97@gmail.com>
2023-03-09 00:42:17 -0300

    feat: creating side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes";
+import { MenuLateral } from "./shared/components/menu-lateral";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 
 export function App() {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <Router />
+        <MenuLateral>
+          <Router />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
---
+++ src/shared/components/menu-lateral/index.tsx
@@ -0,0 +1,62 @@
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+interface MenuLateralProps {
+  children: React.ReactNode;
+}
+
+export function MenuLateral({ children }: MenuLateralProps) {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer open={true} variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://avatars.githubusercontent.com/u/95880342?v=4"
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página Inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+}
