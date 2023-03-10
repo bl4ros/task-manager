cbaf2fedf5bef0ac93f06cf88a55896fd58de0eb
fe-nunes <fernando.fnn97@gmail.com>
2023-03-09 22:59:29 -0300

    feat: Adding loading in category listing

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,15 +1,18 @@
 import {
+  LinearProgress,
   Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
 } from "@mui/material";
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components/ferramentas-da-listagem";
+import { Environment } from "../../shared/environment";
 import { useDebounce } from "../../shared/hooks/useDebouce";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { TasksService, IListagemTasks } from "../../shared/services/api/tasks";
@@ -76,6 +79,20 @@ export function ListagemDeTasks() {
               </TableRow>
             ))}
           </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
         </Table>
       </TableContainer>
     </LayoutBaseDePagina>
