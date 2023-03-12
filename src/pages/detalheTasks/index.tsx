bbde2dd45abe3355dce557fc5f23414816e4be1d
fe-nunes <fernando.fnn97@gmail.com>
2023-03-12 16:38:03 -0300

    feat: backend log query

--- src/pages/detalheTasks/index.tsx
+++ src/pages/detalheTasks/index.tsx
@@ -1,18 +1,52 @@
+import { LinearProgress } from "@mui/material";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { FerramentasDeDetalhes } from "../../shared/components/ferramentas-de-detalhe";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { TasksService } from "../../shared/services/api/tasks";
 
 export function DetalheDeTasks() {
   const { id = "nova" } = useParams<"id">();
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [content, setContent] = React.useState("");
+
+  React.useEffect(() => {
+    if (id !== "nova") {
+      setIsLoading(true);
+      TasksService.getById(Number(id)).then((result) => {
+        setIsLoading(false);
+
+        if (result instanceof Error) {
+          alert(result.message);
+          navigate("/tasks");
+        } else {
+          setContent(result.content);
+          console.log(result);
+        }
+      });
+    }
+  }, [id, navigate]);
+
   const handleSave = () => {};
 
-  const handleDelete = () => {};
+  const handleDelete = (id: number) => {
+    if (window.confirm("Realmente deseja apagar?")) {
+      TasksService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert("Task apagada com sucesso!");
+          navigate("/tasks");
+        }
+      });
+    }
+  };
 
   return (
     <LayoutBaseDePagina
-      titulo="Detalhe da task"
+      titulo={id === "nova" ? "Nova task" : content}
       barraDeFerramentas={
         <FerramentasDeDetalhes
           textoBotaoNovo="Nova"
@@ -22,11 +56,12 @@ export function DetalheDeTasks() {
           aoCLicarEmSalvar={handleSave}
           aoCLicarEmSalvarEFechar={handleSave}
           aoCLicarEmNovo={() => navigate("/tasks/detalhe/nova")}
-          aoCLicarEmApagar={handleDelete}
+          aoCLicarEmApagar={() => handleDelete(Number(id))}
           aoCLicarEmVoltar={() => navigate("/tasks")}
         />
       }
     >
+      {isLoading && <LinearProgress variant="indeterminate" />}
       Detalhe de tasks
     </LayoutBaseDePagina>
   );
